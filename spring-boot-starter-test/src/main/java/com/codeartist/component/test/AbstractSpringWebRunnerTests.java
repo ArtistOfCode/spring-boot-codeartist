2b6141f94504fdad2c72540adfaf025044a1c247
AiJiangnan <aijiangnan@webull.com>
2025-03-20 15:09:26 +0800

    修改单元测试

--- spring-boot-starter-test/src/main/java/com/codeartist/component/test/AbstractSpringWebRunnerTests.java
+++ spring-boot-starter-test/src/main/java/com/codeartist/component/test/AbstractSpringWebRunnerTests.java
@@ -2,18 +2,26 @@ package com.codeartist.component.test;
 
 
 import com.codeartist.component.core.entity.enums.Environments.Profiles;
+import com.codeartist.component.core.util.JSON;
 import com.codeartist.component.test.mock.MockAutoConfiguration;
+import com.github.javafaker.Faker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 /**
  * Web环境基础测试类
@@ -26,14 +34,55 @@ import java.nio.charset.StandardCharsets;
 @Import(MockAutoConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class AbstractSpringWebRunnerTests {
+public abstract class AbstractSpringWebRunnerTests {
+
+    protected static final Faker faker = new Faker();
 
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     protected MockMvc mockMvc;
 
-    protected ResultHandler print() {
-        return result -> logger.info(result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+    protected Consumer<MockHttpServletRequestBuilder> getMockHttpRequestConsumer() {
+        return builder -> {
+        };
+    }
+
+    protected MockHttpServletRequestBuilder doGet(String uri, Object... uriVars) {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get(uri, uriVars);
+        getMockHttpRequestConsumer().accept(builder);
+        return builder;
+    }
+
+    protected MockHttpServletRequestBuilder doPost(String uri, Object body, Object... uriVars) {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post(uri, uriVars)
+                .contentType(MediaType.APPLICATION_JSON);
+        Optional.ofNullable(body).ifPresent(b -> builder.content(JSON.toJSONString(b)));
+        getMockHttpRequestConsumer().accept(builder);
+        return builder;
+    }
+
+    protected MockHttpServletRequestBuilder doPut(String uri, Object body, Object... uriVars) {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put(uri, uriVars)
+                .contentType(MediaType.APPLICATION_JSON);
+        Optional.ofNullable(body).ifPresent(b -> builder.content(JSON.toJSONString(b)));
+        getMockHttpRequestConsumer().accept(builder);
+        return builder;
+    }
+
+    protected MockHttpServletRequestBuilder doDelete(String uri, Object... uriVars) {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.delete(uri, uriVars);
+        getMockHttpRequestConsumer().accept(builder);
+        return builder;
+    }
+
+    protected ResultActions request(RequestBuilder builder) {
+        try {
+            ResultActions actions = mockMvc.perform(builder);
+            actions.andDo(result -> logger.info(result.getResponse().getContentAsString(StandardCharsets.UTF_8)));
+            return actions;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
